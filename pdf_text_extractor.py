ae03db724643461bd69838fa917292c33451eeef
Tapan Yawalkar <tyawalkar3@gmail.com>
2021-06-24 13:29:27 +0530

    Update pdf_text_extractor.py

--- pdf_text_extractor.py
+++ pdf_text_extractor.py
@@ -13,7 +13,7 @@ import time
 
 def convert_pdf_to_text():
     
-    resume_files = glob.glob(r'C:\Users\tapan\Documents\Tapan\Tapan_Resume/' + '*.pdf')
+    resume_files = glob.glob("resume-miner\Resumes\*.pdf")
     resume_text = []
     for resume in resume_files:
         parsed_file = parser.from_file(resume)
@@ -25,11 +25,7 @@ def convert_pdf_to_text():
     
 
 
-if __name__ == "__main__":
-    start_time = time.time()
-    resume_data_in_text = convert_pdf_to_text()
-    end_time = time.time()
-    print("total time for text generation {}".format(end_time-start_time))
+
     
     
    
